de586e219d4809a26b0239d91e4d5232da896cb2
daliamaarek <daliamaarek@gmail.com>
2015-04-22 00:03:19 +0200

    beginning control class

--- src/mips/Control.java
+++ src/mips/Control.java
@@ -3,13 +3,13 @@ package mips;
 public class Control {
 	private int RegDst; // determines if we have an RD?, goes into the register
 						// class
-	private int Branch; //does it branch?
-	private int MemToRead; //read from memory?
-	private int MemToReg; //put into reg
+	private int Branch; // does it branch?
+	private int MemToRead; // read from memory?
+	private int MemToReg; // put into reg
 	private String ALUOp; // two bits for the ALUControl
-	private int MemWrite;	//write into memory
-	private int ALUSrc;	//to determine where the second ALU input is from 
-	private int RegWrite;	//will we write to reg?
+	private int MemWrite; // write into memory
+	private int ALUSrc; // to determine where the second ALU input is from
+	private int RegWrite; // will we write to reg?
 
 	public String getALUOp() {
 		return ALUOp;
@@ -73,5 +73,79 @@ public class Control {
 
 	public void setRegWrite(int regWrite) {
 		RegWrite = regWrite;
+	}
+
+	public Control(String opcode) {
+		switch (opcode) {
+		// all R-Instructions
+		case "000000":
+			RegDst = 1;
+			Branch = 0;
+			MemToRead = 0;
+			MemToReg = 0;
+			ALUOp = "10";
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 1;
+			break;
+		// addi
+		case "001000":
+			RegDst = 0;
+			Branch = 0;
+			MemToRead = 0;
+			MemToReg = 0;
+			ALUOp = "00";
+			MemWrite = 0;
+			ALUSrc = 1;
+			RegWrite = 1;
+			break;
+		// ori
+		case "001101":
+			RegDst = 0;
+			Branch = 0;
+			MemToRead = 0;
+			MemToReg = 0;
+			ALUOp = "11";
+			MemWrite = 0;
+			ALUSrc = 1;
+			RegWrite = 1;
+			break;
+		// lw
+		case "100011":
+			RegDst = 0;
+			Branch = 0;
+			MemToRead = 1;
+			MemWrite = 0;
+			MemToReg = 1;
+			ALUOp = "00";
+			ALUSrc = 1;
+			RegWrite = 1;
+			break;
+		// sw
+		case "101011":
+			Branch = 0;
+			MemToRead = 1;
+			ALUOp = "00";
+			MemWrite = 1;
+			ALUSrc = 1;
+			RegWrite = 0;
+			break;
+		// beq
+		case "000100":
+			Branch = 1;
+			MemToRead = 0;
+			ALUOp = "01";
+			MemWrite = 0;
+			ALUSrc = 0;
+			RegWrite = 0;
+			break;
+		// j
+		case "000010":
+			Branch = 0;
+			MemToRead = 0;
+			MemWrite = 0;
+			RegWrite = 0;
+			break;
+		}
 	}
 }
