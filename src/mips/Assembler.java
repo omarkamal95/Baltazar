b66c315ca6666f2fa87658fce02b399ae6037463
daliamaarek <daliamaarek@gmail.com>
2015-04-21 16:24:49 +0200

    starting to write assembler

--- src/mips/Assembler.java
+++ src/mips/Assembler.java
@@ -1,15 +1,62 @@
 package mips;
 
 public class Assembler {
-		static //opcode, register number, 
-		String [] instructionArray = new String[4]; 
-		public static void assemble (String instruction){
-			instructionArray = instruction.split(",");
+	// opcode, register number,
+	private static String functionCode;
+	private static String opcode;
+	private static String function;
+	private static String input1;
+	private static String input2;
+	private static String input3;
+	static String[] instructionArray = new String[4];
+
+	public static void assemble(String instruction) {
+		instructionArray = instruction.split(",");
+		function = instructionArray[0];
+		input1 = instructionArray[1];
+		input2 = instructionArray[2];
+		input3 = instructionArray[3];
+	}
+
+	public static void toOpcode() {
+		switch (function.toLowerCase()) {
+		case "add": {
+			functionCode = "100000";
+			opcode = "000000";
 		}
-		public static void main(String []args){
-			assemble("add,$t4,$t0,$t2");
-			for(int i =0;i<instructionArray.length; i++){
-				System.out.println(instructionArray[i]);
-			}
+		case "sub": {
+			functionCode = "100010";
+			opcode = "000000";
 		}
+		case "and": {
+			functionCode = "100100";
+			opcode = "000000";
+		}
+		case "nor": {
+			functionCode = "100111";
+			opcode = "000000";
+		}
+		case "or": {
+			functionCode = "100101";
+			opcode = "000000";
+		}
+		case "slt": {
+			functionCode = "101010";
+			opcode = "000000";
+		}
+		case "sll": {
+			functionCode = "000000";
+			opcode = "000000";
+		}
+		case "slr": {
+			functionCode = "000010";
+			opcode = "000000";
+		}
+		case "addi":{
+			opcode = "001000";
+		}
+		
+		}
+	}
+
 }
