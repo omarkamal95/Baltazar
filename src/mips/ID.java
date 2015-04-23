37ce9b058aa176d4a4ddc1a215963d4094d5e58a
Mohamed Yasser <mohamed.yasser.abdel.razek@gmail.com>
2015-04-23 19:32:23 +0200

    Decode inshallah

--- src/mips/ID.java
+++ src/mips/ID.java
@@ -1,38 +1,81 @@
 package mips;
 
+import java.util.HashMap;
+
+import com.sun.org.apache.xalan.internal.xsltc.dom.LoadDocument;
+
 public class ID {
 
-	private Registers registers = new Registers();
 	private String WriteReg;
-	private int DataOne;
-	private int DataTwo;
+	private static String rs;
+	private static String rt;
+	private static String rd;
+	private static String opcode;
+	private static String offset;
+	private static String functionCode;
+	private static String signExtended;
 
-	public int getDataOne() {
-		return DataOne;
+	public String getRs() {
+		return rs;
 	}
 
-	public int getDataTwo() {
-		return DataTwo;
-	}
-
-	public Registers getRegisters() {
-		return registers;
+	public String getRt() {
+		return rt;
 	}
 
 	public String getWriteReg() {
 		return WriteReg;
 	}
 	
-	public ID(String RegOne, String RegTwo, String regDst, String toMuxOne,
-			String WriteData) {
-
-		if (Integer.parseInt(regDst) == 1) {
-			WriteReg = toMuxOne;
-		} else {
-			WriteReg = RegTwo;
+	public static void getDataFromPipline() {
+		HashMap<String,String> IFID = Piplining.getIFID();
+		String instruction = IFID.get("instruction");
+		opcode = instruction.substring(0,5);
+		rs = instruction.substring(6, 10);
+		rt = instruction.substring(11, 15);
+		offset = instruction.substring(16);
+		rd = instruction.substring(16, 20);
+		functionCode = instruction.substring(26);		
+	}
+	
+	public static void loadDataFromRegisters() {
+		HashMap<String,String> IFID = Piplining.getIFID();
+		HashMap<String, String> IDEX = Piplining.getIDEX();
+		// Shoof haneb3at el opcode lel controller ezay
+		IFID.put("Opcode" , opcode);
+		IFID.put("Rs", rs);
+		IFID.put("Rt", rt);
+		
+		String newRS = Integer.toString(Simulator.registers.getRegValue(rs));
+		String newRT = Integer.toString(Simulator.registers.getRegValue(rt));
+		
+		signExtended = signExtend(offset);
+		
+		IDEX.put("Rs", newRS);
+		IDEX.put("Rt", newRT);
+		IDEX.put("Rd", rd);
+		IDEX.put("PC", IDEX.get("PC"));
+		IDEX.put("SignExtended", signExtended);
+		IDEX.put("Function", functionCode);
+		
+	}
+	
+	public static String signExtend(String toBeExtended) {
+		char extend = toBeExtended.charAt(0);
+			for(int i = 0; i < 16; i++) {
+				toBeExtended = extend + toBeExtended;
+			}
+			return toBeExtended;
 		}
-		DataOne = registers.getRegValue(RegOne);
-		DataTwo = registers.getRegValue(RegTwo);
+	
+	public ID() {
 
+		
+	}
+	
+	public static void Decode() {
+		getDataFromPipline();
+		loadDataFromRegisters();
+		
 	}
 }
